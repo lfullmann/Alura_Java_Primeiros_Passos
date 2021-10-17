b3388181a9154523d0206fb14d88f7aeec2adf39
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-10-17 18:06:54 -0300

    Removendo e adicionando itens do array

--- 6-listas.js
+++ 6-listas.js
@@ -9,3 +9,8 @@ listaDeDestinos.push(`Curitiba`); //Push = Adiciona item a lista já declarada
 console.log("Destinos possíveis");
 console.log(listaDeDestinos);
 // console.log(salvador, saoPaulo, rioDeJaneiro);
+
+listaDeDestinos.splice(1, 1); //Splice = Usado para remover item da lista (Indice, quantidade de itens)
+listaDeDestinos.unshift(`Cabo Frio`); // Unshift = Adiciona item no início do array
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[1], listaDeDestinos[3]);
