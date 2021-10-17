40341353aad7f74e15533c9f1690a06ed4c0064c
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-10-17 18:48:14 -0300

    IFs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -2,20 +2,20 @@ console.log(`Trabalhando com condicionais`);
 
 const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
-const idadeComprador = 21;
+const idadeComprador = 17;
+const estaAcompanhada = true;
+
 console.log("Destinos possíveis");
 console.log(listaDeDestinos);
 
 if (idadeComprador >= 18) {
   console.log("Comprador maior de idade");
   listaDeDestinos.splice(1, 1); //Removendo item
+} else if (estaAcompanhada) {
+  console.log("Comprador está acompanhado");
+  listaDeDestinos.splice(1, 1); //Removendo item
 } else {
   console.log("Comprador menor de idade");
 }
-console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador == 18);
+console.log(listaDeDestinos);
