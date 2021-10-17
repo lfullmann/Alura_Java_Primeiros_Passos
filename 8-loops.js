1eeebf27b0d9c0699959218b9a5c88df3e1539ce
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-10-17 20:14:11 -0300

    For

--- 8-loops.js
+++ 8-loops.js
@@ -5,7 +5,7 @@ const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 const idadeComprador = 21;
 const estaAcompanhada = false;
 let temPassagemComprada = false;
-const destino = "Salvador";
+const destino = "Curitiba";
 
 console.log("\n Destinos possíveis");
 console.log(listaDeDestinos);
@@ -22,3 +22,15 @@ while (contador < 3) {
   contador += 1;
 }
 console.log("Destino existe: ", destinoExiste);
+
+if (podeComprar && destinoExiste) {
+  console.log("Boa viagem");
+} else {
+  console.log("Desculpe tivemos um erro!");
+}
+
+for (let i = 0; i < 3; i++) {
+  if (listaDeDestinos[i] == destino) {
+    destinoExiste = true;
+  }
+}
