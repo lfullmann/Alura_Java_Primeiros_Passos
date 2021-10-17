e63fd8fe2ad1c91b3d5806034fb0600db964e68f
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-10-17 17:49:04 -0300

    Criando listas

---
+++ 6-listas.js
@@ -0,0 +1,11 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+
+listaDeDestinos.push(`Curitiba`); //Push = Adiciona item a lista já declarada
+console.log("Destinos possíveis");
+console.log(listaDeDestinos);
+// console.log(salvador, saoPaulo, rioDeJaneiro);
