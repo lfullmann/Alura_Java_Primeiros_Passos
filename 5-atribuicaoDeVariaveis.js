773ec62e4c768bdb5b5588b873443b1497769f5f
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-10-17 17:17:33 -0300

    Atribuição e imutabilidade (Var e Let)

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 33; //Const = variável constante
+let primeiroNome = "Filippe"; // Let = variável mutável
+const sobreNome = "Ullmann";
+
+console.log(primeiroNome, sobreNome);
+console.log(`Meu nome é ${primeiroNome} ${sobreNome}`);
+
+const nomeCompleto = primeiroNome + "" + sobreNome;
+
+console.log(nomeCompleto);
